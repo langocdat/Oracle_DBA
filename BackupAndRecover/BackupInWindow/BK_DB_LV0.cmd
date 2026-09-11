dd95a5bf583bc300d472f9a4ed1c47632d6fa9cd
Lã Ngọc Đạt <86463062+langocdat@users.noreply.github.com>
2026-09-11 22:18:00 +0700

    Add files via upload

--- BackupAndRecover/BackupInWindow/BK_DB_LV0.cmd
+++ BackupAndRecover/BackupInWindow/BK_DB_LV0.cmd
@@ -1,21 +1,21 @@
-@echo off
-set ORACLE_HOME=C:\app\Administrator\product\12.2.0\dbhome_1
-set ORACLE_SID=mvsvc2
+﻿@echo off
+set ORACLE_HOME=C:\app\oracle\product\19c\dbhome_1
+set ORACLE_SID=orcl
 set PATH=%ORACLE_HOME%\bin;%PATH%
 
 set NLS_DATE_FORMAT=YYYY-MM-DD HH24:MI:SS
 
 for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd"') do set TODAYDATE=%%i
 
-set BASEDIR=F:\Backup\LV1_%TODAYDATE%
+set BASEDIR=F:\Backup\LV0_%TODAYDATE%
 set BKDIR=%BASEDIR%\BACKUPFILES
 set LOGDIR=%BASEDIR%\LOGS
 
 if not exist "%BKDIR%" mkdir "%BKDIR%"
 if not exist "%LOGDIR%" mkdir "%LOGDIR%"
 
-set LOGFILE=%LOGDIR%\LogBK_DB_LV1_%TODAYDATE%.log
-set RMANSCRIPT=%LOGDIR%\BK_DB_LV1_%TODAYDATE%.rman
+set LOGFILE=%LOGDIR%\LogBK_DB_LV0_%TODAYDATE%.log
+set RMANSCRIPT=%LOGDIR%\BK_DB_LV0_%TODAYDATE%.rman
 
 echo start >> "%LOGFILE%"
 echo %DATE% %TIME% >> "%LOGFILE%"
@@ -38,22 +38,25 @@ echo    allocate channel d7 device type disk;
 echo    allocate channel d8 device type disk;
 echo.
 echo    backup as compressed backupset
-echo      incremental level 1 cumulative database
-echo      format '%BKDIR%\datafile_LV1_%%d_%%T_%%U.bkp'
-echo      tag='DB_LV1_%TODAYDATE%';
+echo      incremental level 0 database
+echo      format '%BKDIR%\datafile_LV0_%%d_%%T_%%U.bkp'
+echo      tag='DB_LV0_%TODAYDATE%';
 echo.
 echo    backup as compressed backupset
 echo      archivelog all
-echo      format '%BKDIR%\arch_LV1_%%d_%%T_%%U.bkp'
-echo      tag='ARC_LV1_%TODAYDATE%';
+echo      format '%BKDIR%\arch_LV0_%%d_%%T_%%U.bkp'
+echo      tag='ARC_LV0_%TODAYDATE%';
 echo.
 echo    backup spfile
-echo      format '%BKDIR%\spfile_LV1_%%d_%%T_%%U.bkp'
-echo      tag='SPFILE_LV1_%TODAYDATE%';
+echo      format '%BKDIR%\spfile_LV0_%%d_%%T_%%U.bkp'
+echo      tag='SPFILE_LV0_%TODAYDATE%';
 echo.
 echo    backup current controlfile
-echo      format '%BKDIR%\controlfile_LV1_%%d_%%T_%%U.bkp'
-echo      tag='CTL_LV1_%TODAYDATE%';
+echo      format '%BKDIR%\controlfile_LV0_%%d_%%T_%%U.bkp'
+echo      tag='CTL_LV0_%TODAYDATE%';
+echo.
+echo    crosscheck archivelog all;
+echo    delete noprompt archivelog until time 'SYSDATE - 15';
 echo.
 echo    release channel d1;
 echo    release channel d2;
